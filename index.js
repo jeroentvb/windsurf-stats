d148f75a9520df2436b7b2a6ddc3ed31921dcb13
jeroentvb <jeroen.van.berkum@hva.nl>
2018-09-13 17:49:15 +0200

    Copied some code from another project to reuse

--- index.js
+++ index.js
@@ -22,6 +22,85 @@ function index(req, res) {
   })
 }
 
+function gatherData(req, res) {
+  var windfinder = {
+    spot: '',
+    windspeed: new Array,
+    windgust: new Array,
+    winddirection: new Array
+  }
+
+  request(options.windfinderUrl, function(error, response, html) {
+    if(error) {
+      res.render('error', {
+        page: 'error',
+        error: error
+      })
+      throw error
+    } else {
+      var $ = cheerio.load(html)
+
+      // Get the spots name
+      $('#spotheader-spotname').filter(function() {
+        windfinder.spot = $(this).text()
+      })
+
+      // Get the dates
+      $('.weathertable__header').find($('h4')).filter(function(i) {
+        windfinder.date[i] = $(this).text()
+      })
+
+      // Get the time
+      $('.data-time').find($('.value')).filter(function(i) {
+        // console.log($(this).text())
+        windfinder.time[i] = $(this).text()
+      })
+      spliceToFirstDay(windfinder.time)
+
+      // Get the average wind speed
+      $('.data--major').find($('.units-ws')).filter(function(i) {
+        windfinder.windspeed[i] = $(this).text()
+      })
+      spliceToFirstDay(windfinder.windspeed)
+
+      // Get the wind gusts
+      $('.data-gusts').find($('.units-ws')).filter(function(i) {
+        windfinder.windgust[i] = $(this).text()
+      })
+      spliceToFirstDay(windfinder.windgust)
+
+      // Get the wind direction; do some converting
+      $('.data-direction-arrow').find($('.directionarrow')).filter(function(i) {
+        var data = parseInt($(this).attr('title').replace('°', ' ')) - 180
+        // This can be used to calculate the wind direction in wind direction instead of angles
+        // var val = Math.floor((data / 22.5) + 0.5)
+        // var windDirections = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+        // windDirection[i] = windDirections[(val % 16)]
+        windfinder.winddirection[i] = data
+      })
+      spliceToFirstDay(windfinder.winddirection)
+    }
+  })
+}
+
+function spliceToFirstDay(array) {
+  // Remove the first 7 hours
+  array.splice(0, 7)
+  // Remove other days
+  array.splice(16, 50)
+}
+
+function spliceToDayHours(array) {
+  // Remove the first 7 hours
+  array.splice(0, 7)
+  // Remove the night between day 1 and 2
+  array.splice(16, 8)
+  // Remove the night between day 2 and 3
+  array.splice(32, 8)
+  // Remove last hour of day 3 (23h)
+  array.splice(48, 10)
+}
+
 function notFound(req, res) {
   res.status(404).render('error', {
     page: 'Error 404',
