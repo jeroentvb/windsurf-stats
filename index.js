efca3e7dde794dcf46757da2cc0b199eed6e3445
jeroentvb <jeroen.van.berkum@hva.nl>
2018-09-13 16:41:24 +0200

    Setting up the file structure

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const request = require('request')
+const cheerio = require('cheerio')
+const chalk = require('chalk')
+const fs = require('fs')
+
+var options = {
+  port: 25561
+}
+
+module.exports = express()
+  .set('view engine', 'ejs')
+  .set('views', 'templates')
+  .use(express.static('static'))
+  .get('/', index)
+  .use(notFound)
+  .listen(options.port, () => console.log(chalk.green(`Server listening on port ${options.port}...`)))
+
+
+function notFound(req, res) {
+  res.status(404).render('error', {
+    page: 'Error 404',
+    error: 'The page was not found'
+  })
+}
