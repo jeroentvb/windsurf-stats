1c59ecbfb761add2a46b1993ca7654d6a6b110c2
jeroentvb <jeroen.van.berkum@hva.nl>
2018-09-21 14:45:52 +0200

    Added a fieldset for manual input of winddata when the session was in the past

--- static/js/script.js
+++ static/js/script.js
@@ -13,4 +13,16 @@ if(document.getElementById('submit-form') != undefined) {
       noteInput.style.backgroundColor = 'red'
     }
   })
+
+  var additionalStats = document.getElementById('additional-stats')
+  var radioToday = document.getElementById('today')
+  var radioOther = document.getElementById('other-day')
+
+  radioOther.addEventListener('click', function() {
+    additionalStats.classList.remove('hidden')
+  })
+
+  radioToday.addEventListener('click', function() {
+    additionalStats.classList.add('hidden')
+  })
 }
