d69f443eff0906b7d1ad6b3bfdd0048f23f25d60
jeroentvb <jeroen.van.berkum@hva.nl>
2018-09-25 21:49:04 +0200

    Added more logic to select custom spot and board (frontend)

--- static/js/script.js
+++ static/js/script.js
@@ -12,41 +12,65 @@ if (document.getElementById('submit-form') != undefined) {
     additionalStats.classList.add('hidden')
   })
 
+  // Function for showing other options
+  function hideShowOther(select, otherInput) {
+    select.addEventListener('change', function() {
+      if (select.value == 'other') {
+        otherInput.classList.remove('hidden')
+      } else {
+        otherInput.classList.add('hidden')
+      }
+    })
+  }
+
+  // Choosing other spot
+  var spot = document.getElementById('spot')
+  var otherSpot = document.getElementById('spot-other')
+
+  hideShowOther(spot, otherSpot)
+
   // Choosing other sail size
   var sailSize = document.getElementById('windsurf-sail-size')
   var otherSailSize = document.getElementById('windsurf-sail-size-other')
 
-  sailSize.addEventListener('change', function() {
-    if (sailSize.value == 'other') {
-      otherSailSize.classList.remove('hidden')
-    } else {
-      otherSailSize.classList.add('hidden')
-    }
-  })
+  hideShowOther(sailSize, otherSailSize)
+
+  // Choosing other board
+  var windsurfBoard = document.getElementById('windsurf-board')
+  var otherWindsurfBoard= document.getElementById('windsurf-board-other')
+
+  hideShowOther(windsurfBoard, otherWindsurfBoard)
 
   // Check form before submitting
   var ratingInput = document.getElementById('rating')
   var noteInput = document.getElementById('note')
   var submitForm = document.getElementById('submit-form')
 
+  // Check input fields that are visible by default
+  function checkInputError(input, e) {
+    if (input.value == '') {
+      e.preventDefault()
+      input.classList.add('error')
+    } else {
+      input.classList.remove('error')
+    }
+  }
+
+  // Check input fields that are NOT visible by default
+  function checkOtherInputError(otherInput, e) {
+    if (otherInput.classList.contains('hidden') == false && otherInput.value == '') {
+      e.preventDefault()
+      otherInput.classList.add('error')
+    } else {
+      otherInput.classList.remove('error')
+    }
+  }
+
   submitForm.addEventListener('click', function(e) {
-    if (ratingInput.value == '') {
-      e.preventDefault()
-      ratingInput.classList.add('error')
-    } else {
-      ratingInput.classList.remove('error')
-    }
-    if (noteInput.value == '') {
-      e.preventDefault()
-      noteInput.classList.add('error')
-    } else {
-      noteInput.classList.remove('error')
-    }
-    if (otherSailSize.classList.contains('hidden') == false && otherSailSize.value == '') {
-      e.preventDefault()
-      otherSailSize.classList.add('error')
-    } else {
-      otherSailSize.classList.remove('error')
-    }
+    checkInputError(ratingInput, e)
+    checkInputError(noteInput, e)
+    checkOtherInputError(otherSpot, e)
+    checkOtherInputError(otherSailSize, e)
+    checkOtherInputError(otherWindsurfBoard, e)
   })
 }
