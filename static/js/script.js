367ca80aed909bd493ed4256bb1613caee3585ac
jeroentvb <jeroen.van.berkum@hva.nl>
2018-11-13 21:40:17 +0100

    Added a hamburger menu for smaller screens

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,17 @@ for (var i = 0; i < a.length; i++) {
     return false
   }
 }
+
+var openMenu = document.getElementById('open-menu')
+var closeMenu = document.getElementById('close-menu')
+var menu = document.getElementById('menu')
+
+function toggleMenu (remove, add) {
+  return function () {
+    menu.classList.remove(remove)
+    menu.classList.add(add)
+  }
+}
+
+openMenu.addEventListener('click', toggleMenu('hide-menu', 'show-menu'))
+closeMenu.addEventListener('click', toggleMenu('show-menu', 'hide-menu'))
