9ad84b3c4ec10453dbc684e0435a4bc90dc6ea55
jeroentvb <jeroen.van.berkum@hva.nl>
2019-01-18 21:18:07 +0100

    Added a demo

---
+++ docs/demo/assets/script.js
@@ -0,0 +1,24 @@
+// Code for links inside a apple-mobile content enabled app
+// var a = document.getElementsByTagName('a')
+// for (var i = 0; i < a.length; i++) {
+//   a[i].onclick = function () {
+//     window.location = this.getAttribute('href')
+//     return false
+//   }
+// }
+
+var openMenu = document.getElementById('open-menu')
+var closeMenu = document.getElementById('close-menu')
+var menu = document.getElementById('menu')
+
+function toggleMenu (remove, add) {
+  return function () {
+    menu.classList.remove(remove)
+    menu.classList.add(add)
+  }
+}
+
+if (document.getElementById('menu')) {
+  openMenu.addEventListener('click', toggleMenu('hide-menu', 'show-menu'))
+  closeMenu.addEventListener('click', toggleMenu('show-menu', 'hide-menu'))
+}
