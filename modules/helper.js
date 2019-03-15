84ec97654f02e27ac58766f929ff3eeee1a74ecc
jeroentvb <jeroentvb@hotmail.com>
2019-03-15 12:54:49 +0100

    Re-added getYesterday function

--- modules/helper.js
+++ modules/helper.js
@@ -10,6 +10,22 @@ function getToday () {
   return `${dd}-${mm}-${yyyy}`
 }
 
+function getYesterday () {
+  const today = new Date()
+  let dd = today.getDate()
+  let mm = today.getMonth() + 1
+  let yyyy = today.getFullYear()
+
+  if (dd < 10) dd = `0${dd}`
+  if (mm < 10) mm = `0${mm}`
+
+  if (dd - 1 === 0) {
+    return `${30}-${mm - 1}-${yyyy}`
+  } else {
+    return `${dd - 1}-${mm}-${yyyy}`
+  }
+}
+
 function localize (lang) {
   const localization = require(`../localization/${lang}.json`)
   return localization
@@ -22,6 +38,7 @@ function getWindDirection (deg, windDirections) {
 
 module.exports = {
   getToday,
+  getYesterday,
   localize,
   getWindDirection
 }
