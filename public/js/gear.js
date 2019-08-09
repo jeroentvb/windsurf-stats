4b78c4b183dd263ab4d970e899efc9e4c3a4be64
jeroentvb <jeroentvb@hotmail.com>
2019-08-09 13:01:15 +0200

    Styled forms for a small desktop

--- public/js/gear.js
+++ public/js/gear.js
@@ -1,21 +1,28 @@
 import { element } from './modules/element.js'
 
-const form = document.getElementsByTagName('form')[0]
+// const form = document.getElementsByTagName('form')[0]
 const fieldset = document.getElementById('gear-sails')
-const button = element.button('Add another sail', 'button--medium')
-let showSail = 5
+const button = element.button('Add another sail', ['button--large', 'add-sail'])
+let showSail = 6
 
-for (let i = 5; i < fieldset.children.length; i++) {
-  fieldset.children[i].classList.add('hidden')
+if (document.body.clientWidth < 767) {
+  for (let i = 6; i < fieldset.children.length; i++) {
+    if (fieldset.children[i].children[1].value === '') {
+      fieldset.children[i].classList.add('hidden')
+    } else {
+      showSail++
+    }
+  }
+
+  // form.insertBefore(button, fieldset.nextElementSibling)
+  fieldset.appendChild(button)
+
+  button.addEventListener('click', e => {
+    e.preventDefault()
+
+    fieldset.children[showSail].classList.remove('hidden')
+    showSail++
+
+    if (showSail === 11) button.remove()
+  })
 }
-
-form.insertBefore(button, fieldset.nextElementSibling)
-
-button.addEventListener('click', e => {
-  e.preventDefault()
-
-  fieldset.children[showSail].classList.remove('hidden')
-  showSail++
-
-  if (showSail === 10) button.remove()
-})
