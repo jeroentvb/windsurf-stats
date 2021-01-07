2330fd7a212269ee545b423efa9d8c7256294add
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2021-01-07 18:12:10 +0100

    refactor(checkLogin): fix checking if route is protected

--- server/src/middleware/check-login.ts
+++ server/src/middleware/check-login.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 import { UNPROTECTED_ROUTES } from '../constants/unprotected-routes'
 
 export default function checkLogin (req: Request, res: Response, next: NextFunction) {
-  const protectedRoute = UNPROTECTED_ROUTES.findIndex((route) => route === req.path) > -1
+  const protectedRoute = UNPROTECTED_ROUTES.findIndex((route) => route === req.path) === -1
 
   if (!req.session!.user && protectedRoute) {
     res.status(401).send()
